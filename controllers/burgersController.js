f490f93144a3477522b2af6223b78bde46e03de2
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-01-11 00:09:22 -0600

    server up

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,11 +1,7 @@
 var express = require("express");
-
 var router = express.Router();
-
-// Import the model (cat.js) to use its database functions.
 var burger = require("../models/burger.js");
 
-// Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.all(function(data) {
     var hbsObject = {
@@ -22,7 +18,7 @@ router.post("/api/burgers", function(req, res) {
   ], [
     req.body.name, req.body.devoured
   ], function(result) {
-    // Send back the ID of the new quote
+   
     res.json({ id: result.insertId });
   });
 });
--- models/burger.js
+++ models/burger.js
@@ -7,7 +7,7 @@ var burger = {
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
+
   create: function(cols, vals, cb) {
     orm.create("burgerss", cols, vals, function(res) {
       cb(res);
@@ -25,5 +25,4 @@ var burger = {
   }
 };
 
-// Export the database functions for the controller (catsController.js).
 module.exports = burger;
