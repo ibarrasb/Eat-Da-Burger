dc109995bfca9ae4b63a9dce1482fedd4dcd6758
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-01-12 00:14:36 -0600

    live

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
+var connection;
+if(process.env.JAWSDB_URL){
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "",
   database: "eat_db"
 });
+}
 
 // Make connection.
 connection.connect(function(err) {
